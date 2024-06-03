4c583be8a90500a752e77bfea260a29d99d71803
Maria Laura <mlauraifg@gmail.com>
2024-06-03 10:40:49 -0300

    Atualizando o Adopt Modal

--- pages/desktop_page/main.js
+++ pages/desktop_page/main.js
@@ -1,15 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const openModalAdopt = document.querySelector("#adopt-modal");
-    const closeModalbtn = document.querySelector("#cancel-btn");
-    const modal = document.querySelector("#modal");
-    const fade = document.querySelector("#fade");
+const modal = document.querySelector("dialog")
+const openModalAdopt = document.querySelector("#adopt-modal");
+const openModalAdopt2 = document.querySelector("#adopt-btn")
+const openModalAdopt3 = document.querySelector("#apply-btn")
+const closeModalAdopt = document.querySelector("#cancel-btn")
 
-    const toggleModal = () => {
-        modal.classList.toggle("hide")
-        fade.classList.toggle("hide")
+const openButtons = [openModalAdopt, openModalAdopt2, openModalAdopt3];
+
+openButtons.forEach((el) => {
+    if (el) {
+        el.addEventListener("click", () => modal.showModal());
     }
-
-    [openModalAdopt, closeModalbtn, fade].forEach((el) => {
-        el.addEventListener("click", () => toggleModal());
-    });
 });
+
+closeModalAdopt.addEventListener("click", () => {
+    modal.close()
+})
+
+// Redireciona para a página Success
+const form = document.getElementById("form");
+const btnApplyAdopt = document.getElementById("ok-btn");
+
+btnApplyAdopt.addEventListener("click", () => {
+    if (form.checkValidity()) {
+        location.href = '../success_page/index.html'
+    } else {
+        form.reportValidity();
+    }
+})
